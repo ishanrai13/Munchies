759ddfe260ea6ccf074b20cd4e20bdd005b107a4
ishanrai13 <ishanbansal012@gmail.com>
2023-01-30 01:47:52 +0530

    nav active class added

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,30 +1,59 @@
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import LOGO_IMG_URL from '../../assets/img/logo.png';
 import * as s from './Header.module.scss';
 
 const Header = () => {
+	let activeStyle = {
+		paddingBottom: '5px',
+		borderBottom: '1px solid green',
+	};
 	return (
 		<div className={s.header}>
 			<div className={s.headerContanier}>
-				<Link to="/">
+				<NavLink to="/">
 					<img src={LOGO_IMG_URL} />
-				</Link>
+				</NavLink>
 				<ul>
-					<Link to="/">
-						<li>Home</li>
-					</Link>
-					<Link to="/about">
-						<li>About Us</li>
-					</Link>
-					<Link to="/cart">
-						<li>Cart</li>
-					</Link>
-					<Link to="/profile">
-						<li>Profile</li>
-					</Link>
-					<Link to="/login">
-						<li>Login</li>
-					</Link>
+					<li>
+						<NavLink
+							to="/"
+							style={({ isActive }) => (isActive ? activeStyle : undefined)}
+						>
+							Home
+						</NavLink>
+					</li>
+					<li>
+						<NavLink
+							to="/about"
+							style={({ isActive }) => (isActive ? activeStyle : undefined)}
+						>
+							About Us
+						</NavLink>
+					</li>
+					<li>
+						<NavLink
+							to="/cart"
+							style={({ isActive }) => (isActive ? activeStyle : undefined)}
+						>
+							Cart
+						</NavLink>
+					</li>
+					<li>
+						<NavLink
+							to="/profile"
+							style={({ isActive }) => (isActive ? activeStyle : undefined)}
+						>
+							Profile
+						</NavLink>
+					</li>
+					<li>
+						<NavLink
+							to="/login"
+							style={({ isActive }) => (isActive ? activeStyle : undefined)}
+						>
+							Login
+						</NavLink>
+					</li>
 				</ul>
 			</div>
 		</div>
